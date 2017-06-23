a0433c642f6a8b2d599dc4b34d4de2ec8866d486
DESKTOP-PJJLTO9\Justin <jmille23@my.westga.edu>
2017-06-22 22:31:52 -0400

    Home page now displays correctly. CSS Needs implemented

--- src/main/resources/static/webapp/js/app.js
+++ src/main/resources/static/webapp/js/app.js
@@ -2,7 +2,7 @@
 
 var appmodule = angular.module('app', ['ngRoute','BookModule']).
 config(['$routeProvider', function ($routeProvider) {	
-	$routeProvider.when('/home', { templateUrl: 'home.html', controller: 'homeController'})
+	$routeProvider.when('/home', { templateUrl: '../html/home.html', controller: 'homeController'})
     .otherwise({ redirectTo: '/home' });
 }]);
 
