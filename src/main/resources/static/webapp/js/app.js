513c0d810480842b2cd4720db87be128a80b027a
DESKTOP-PJJLTO9\Justin <jmille23@my.westga.edu>
2017-06-22 22:45:01 -0400

    Place holder addcomp created, Add Complete now redirects correctly.

--- src/main/resources/static/webapp/js/app.js
+++ src/main/resources/static/webapp/js/app.js
@@ -4,6 +4,8 @@ var appmodule = angular.module('app', ['ngRoute','BookModule']).
 config(['$routeProvider', function ($routeProvider) {	
 	$routeProvider.when('/home', { templateUrl: '../html/home.html', controller: 'homeController'})
     .otherwise({ redirectTo: '/home' });
+	$routeProvider.when('/addcomp', { templateUrl: '../html/addcomp.html', controller: 'homeController'})
+    .otherwise({ redirectTo: '/home' });
 }]);
 
 angular.module('BookModule', []);
