624432fc79c741b736328c87f5ca8773a9091039
David Bennett <dbennet4@my.westga.edu>
2017-07-09 19:20:53 -0500

    The BookService tests now pass.

--- src/test/java/com/edu/westga/cs6920/wiseowl/tests/BookServiceTest.java
+++ src/test/java/com/edu/westga/cs6920/wiseowl/tests/BookServiceTest.java
@@ -1,16 +1,24 @@
 package com.edu.westga.cs6920.wiseowl.tests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import com.edu.westga.cs6920.wiseowl.model.Book;
-import com.edu.westga.cs6920.wiseowl.model.BookFormat;
-import com.edu.westga.cs6920.wiseowl.model.BookSource;
 import com.edu.westga.cs6920.wiseowl.model.User;
 import com.edu.westga.cs6920.wiseowl.service.BookService;
 
@@ -22,8 +30,40 @@ import com.edu.westga.cs6920.wiseowl.service.BookService;
  */
 public class BookServiceTest {
 
-	private BookService bookservice;
 	private Book book;
+	private List<Book> booklist;
+	private HttpServletRequest request;
+	private HttpSession session;
+	private User user;
+	private TypedQuery<Book> query;
+	
+	@Mock
+	private EntityManager em;
+	
+	@InjectMocks
+	private BookService bookservice = new BookService();
+	
+	@Before
+	public void setup() throws Exception
+	{     
+	    this.request = mock(HttpServletRequest.class);
+	    this.session = mock(HttpSession.class);  
+	    this.book = new Book("Title");
+	    this.booklist = new ArrayList<Book>();
+		this.booklist.add(this.book);
+		this.query = mock(TypedQuery.class);
+	    
+	    MockitoAnnotations.initMocks(this);
+	    
+	    when(this.request.getSession()).thenReturn(session);
+	    when(this.session.getAttribute("loggedUser")).thenReturn(this.user);
+	    when(this.em.createNamedQuery("Book.getCompBooksQuery", Book.class)).thenReturn(this.query);
+	    when(this.em.createNamedQuery("Book.getReviewedBooksQuery", Book.class)).thenReturn(this.query);
+	    when(this.em.find(Book.class, new Long(this.book.getBook_ID()))).thenReturn(this.book);
+	    when(query.setParameter("username", "test1")).thenReturn(query);
+	    when(query.setMaxResults(Mockito.anyInt())).thenReturn(query);
+	    when(query.getResultList()).thenReturn(this.booklist);
+	}
 	
 	/**
 	 * Test to make sure the addCompBook method properly returns the book given to it.
@@ -31,8 +71,6 @@ public class BookServiceTest {
 	 */
 	@Test
 	public void testAddCompBook() throws Exception {
-		this.bookservice = new BookService();
-		this.book = new Book("Title");
 		assertEquals(this.book, this.bookservice.addCompBook(this.book));
 	}
 	
@@ -42,10 +80,8 @@ public class BookServiceTest {
 	 */
 	@Test
 	public void testGetCompBooks() throws Exception {
-		this.bookservice = new BookService();
-		this.book = new Book("Title");
 		this.bookservice.addCompBook(this.book);
-		assertFalse(this.bookservice.getCompBooks("jatin", 1).isEmpty());
+		assertEquals(this.booklist, this.bookservice.getCompBooks("test1", 1));
 	}
 	
 	/**
@@ -54,11 +90,9 @@ public class BookServiceTest {
 	 */
 	@Test
 	public void testGetReviewedBooks() throws Exception {
-		this.bookservice = new BookService();
-		this.book = new Book("Title");
 		this.book.setBook_read_rating("5");
 		this.bookservice.addCompBook(this.book);
-		assertFalse(this.bookservice.getReviewedBooks("jatin", 1).isEmpty());
+		assertEquals(this.booklist, this.bookservice.getReviewedBooks("test1", 1));
 	}
 	
 	/**
@@ -67,10 +101,8 @@ public class BookServiceTest {
 	 */
 	@Test
 	public void testGetBookById() throws Exception {
-		this.bookservice = new BookService();
-		this.book = new Book("Title");
 		this.bookservice.addCompBook(this.book);
-		assertNotNull(this.bookservice.getBookById(1));
+		assertEquals(this.book, this.bookservice.getBookById((int) this.book.getBook_ID()));
 	}
 	
 	/**
@@ -79,8 +111,6 @@ public class BookServiceTest {
 	 */
 	@Test
 	public void testUpdateBook() throws Exception {
-		this.bookservice = new BookService();
-		this.book = new Book("Title");
-		assertEquals("Title", this.bookservice.updateBook(this.book));
+		assertEquals(this.book, this.bookservice.updateBook(this.book));
 	}
 }
